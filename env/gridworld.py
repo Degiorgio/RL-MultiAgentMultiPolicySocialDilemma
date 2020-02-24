172e8ed0c7e77f2c8517f05f401db0f30b425bd4
Kurt Degiorgio <degiorgiokurt@outlook.com>
2020-02-24 01:42:23 +0000

    removed stuff

--- env/gridworld.py
+++ env/gridworld.py
@@ -3,6 +3,8 @@ import cv2
 from PIL import Image
 from env.blob import Blob
 import collections
+import gym
+from gym.spaces import Discrete, Box
 
 Point = collections.namedtuple('Point', 'x y')
 
@@ -57,23 +59,28 @@ class Player(Blob):
             f" {self.dead}, facing_dir: {self.facing_dir % 4}"
 
 
-class BlobEnv:
+class GatheringEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
     def __init__(self,
                  size=20,
-                 return_images=True,
+                 num_players=2,
                  max_steps=1000,
+                 player_move_cost=1,
                  player_respawn_time=5,
                  player_murder_mode=True,
+                 food_reward=25,
                  food_respawn_time=20,
                  food_level=FOOD_NORMAL,
                  draw_shooting_direction=True,
                  logger_callback=None):
+
         self.size = size
-        self.return_images = return_images
         self.max_steps = max_steps
+        self.num_players = num_players
 
         self.players = None
-        self.player_move_cost = 1
+        self.player_move_cost = player_move_cost
         self.murder_mode = player_murder_mode
         self.player_respawn_time = player_respawn_time
         self.draw_shooting_direction = draw_shooting_direction
@@ -81,12 +88,16 @@ class BlobEnv:
         self.food = None
         self.food_level = food_level
         self.food_respawn_time = food_respawn_time
-        self.food_reward = 25
+        self.food_reward = food_reward
         self.logger_callback = logger_callback
+
+        # GYM stuff
+        self.action_space = Discrete(self._get_action_space())
+        self.observation_space = Box(0, 255, [size, size, 3])
 
         if self.logger_callback is not None:
             self.logger_callback(
-                f"Env HP: size: {size}, max_steps: {self.max_steps}, draw_shooting_direction: {draw_shooting_direction}, num_actions: {self.get_n_actions()}")
+                f"Env HP: size: {size}, max_steps: {self.max_steps}, draw_shooting_direction: {draw_shooting_direction}, num_actions: {self.action_space()}")
             self.logger_callback(
                 f"Reward HP: player_move_cost: {self.player_move_cost}, food_reward: {self.food_reward}")
             self.logger_callback(
@@ -94,59 +105,28 @@ class BlobEnv:
             self.logger_callback(
                 f"Player HP: player_respawn_time: {self.player_respawn_time}, player_murder_mode: {self.murder_mode}")
 
-    def get_n_actions(self):
-        if self.murder_mode:
-            return 8
-        else:
-            return 5
-
-    def get_observation_space(self):
-        if self.return_images:
-            return (self.size, self.size, 3)
-        else:
-            raise NotImplementedError("only images supported")
-
-
-    def generate_food(self, start=np.array([0, 0])):
-        size = self.food_level
-        tl = size * 2 - 1
-        top = start + tl - 1
-        for idx in range(size - 1):
-            for i in range(idx * 2 + 1):
-                row = top[0] - idx
-                col = start[1] + size - 1 - idx + i
-                self.food.append(Apple(size=self.size, x=row, y=col))
-        for idx in range(size - 1, -1, -1):
-            for i in range(idx * 2 + 1):
-                row = start[0] + idx
-                col = start[1] + size - 1 - idx + i
-                self.food.append(Apple(size=self.size, x=row, y=col))
-
-    def _add_player(self, color):
-        player = Player(len(self.players), self.size, color=color, facing_dir=0)
-        while player in self.food or player in self.players:
-            player = Player(len(self.players), self.size, color=color, facing_dir=0)
-        self.players.append(player)
-
-    def reset(self, num_players=2):
+    def reset(self):
         self.players = []
         self.food = []
         self.episode_step = 0
 
         # generate food
-        self.generate_food(
+        self._generate_food(
             start=np.array([(self.size//2)-5, (self.size//2)-5]))
 
         # generate players
         self._add_player(color=(255, 0, 0))
-        if num_players == 2:
+        if self.num_players == 2:
             self._add_player(color=(0, 0, 255))
-        elif num_players != 1:
+        elif self.num_players != 1:
             raise NotImplementedError("only 1-2 players supported")
 
-        if self.return_images:
-            observation = np.array(self.get_image())
-        return observation
+        observations = []
+        observation = np.array(self._get_image())
+        for player in self.players:
+            observations.append(observation.copy())
+
+        return observations
 
     def step(self, actions):
         assert(len(actions) == len(self.players))
@@ -193,29 +173,61 @@ class BlobEnv:
                     apple.respawn()
 
         # compute new_observation
-        if self.return_images:
-            new_observation = np.array(self.get_image())
-        else:
-            raise NotImplementedError("only images supported")
+        new_observations = []
+        new_observation = np.array(self._get_image())
+        for player in self.players:
+            new_observations.append(new_observation.copy())
 
         # check if game is finished
         done = False
         if self.episode_step >= self.max_steps:
             done = True
 
-        return new_observation, rewards, done
+        return new_observations, rewards, done, {}
 
-    def render(self, wait=True):
-        print("env rendering")
-        img = self.get_image()
-        # resizing so we can see
-        img = img.resize((300, 300))
-        cv2.imshow("image", np.array(img))
-        if wait:
-            cv2.waitKey(0)
+    def render(self, mode="human", close=False):
+        if mode != "human":
+            raise Exception("unsupported render mode: " + mode)
+        if close:
             cv2.destroyAllWindows()
+        else:
+            img = self._get_image()
+            img = img.resize((300, 300))
+            cv2.imshow("image", np.array(img))
+            cv2.waitKey(0)
 
-    def contains_food(self, x, y):
+    def _get_action_space(self):
+        if self.murder_mode:
+            return 8
+        else:
+            return 5
+
+    def _generate_food(self, start=np.array([0, 0])):
+        size = self.food_level
+        tl = size * 2 - 1
+        top = start + tl - 1
+        for idx in range(size - 1):
+            for i in range(idx * 2 + 1):
+                row = top[0] - idx
+                col = start[1] + size - 1 - idx + i
+                self.food.append(Apple(size=self.size, x=row, y=col))
+        for idx in range(size - 1, -1, -1):
+            for i in range(idx * 2 + 1):
+                row = start[0] + idx
+                col = start[1] + size - 1 - idx + i
+                self.food.append(Apple(size=self.size, x=row, y=col))
+
+    def _add_player(self, color):
+        player = Player(
+            len(self.players), self.size, color=color, facing_dir=0
+        )
+        while player in self.food or player in self.players:
+            player = Player(
+                len(self.players), self.size, color=color, facing_dir=0
+            )
+        self.players.append(player)
+
+    def _contains_food(self, x, y):
         point = Point(x=x, y=y)
         if point in self.food:
             food_index = self.food.index(point)
@@ -223,7 +235,7 @@ class BlobEnv:
                 return True
         return False
 
-    def contains_player(self, x, y, pid):
+    def _contains_player(self, x, y, pid):
         point = Point(x=x, y=y)
         if point in self.players:
             pindex = self.players.index(point)
@@ -232,7 +244,7 @@ class BlobEnv:
         else:
             return False
 
-    def contains_player_direction(self, x, y):
+    def _contains_player_direction(self, x, y):
         if self.murder_mode and self.draw_shooting_direction:
             for player in self.players:
                 if not player.dead:
@@ -241,7 +253,7 @@ class BlobEnv:
                         return True
         return False
 
-    def get_image(self):
+    def _get_image(self):
         # starts an rbg of our size
         env = np.zeros((self.size, self.size, 3), dtype=np.uint8)
 
--- test_env.py
+++ test_env.py
@@ -1,5 +1,5 @@
 import arcade
-from env.gridworld import BlobEnv
+from env.gridworld import GatheringEnv
 
 # actions
 from env.blob import UP, DOWN, LEFT, RIGHT, NOTHING
@@ -12,22 +12,25 @@ MARGIN = 1
 SCREEN_WIDTH = (WIDTH + MARGIN) * GRID_WORLD_SIZE + MARGIN
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * GRID_WORLD_SIZE + MARGIN
 
-NUM_PLAYERS = 1
+NUM_PLAYERS = 2
 
 
 class Gathering(arcade.Window):
     def __init__(self):
         if NUM_PLAYERS == 1:
-            self.env = BlobEnv(size=20, player_murder_mode=False)
+            self.env = GatheringEnv(size=20,
+                                    num_players=NUM_PLAYERS,
+                                    player_murder_mode=False)
         else:
-            self.env = BlobEnv(size=20, murder_mode=True)
-
+            self.env = GatheringEnv(size=20,
+                                    num_players=NUM_PLAYERS,
+                                    player_move_cost=True)
         self.set_update_rate(1 / 10)
         self.acted = None
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     def setup(self):
-        self.env.reset(num_players=NUM_PLAYERS)
+        self.env.reset()
         print(self.env.players)
 
     def update(self, dt):
@@ -63,6 +66,8 @@ class Gathering(arcade.Window):
             action = NOTHING
         elif key == arcade.key.ENTER:
             self.env.render()
+        elif key == arcade.key.BACKSPACE:
+            self.env.render(close=True)
         elif key == arcade.key.SPACE:
             action = SHOOT
         elif key == arcade.key.HOME:
@@ -70,14 +75,16 @@ class Gathering(arcade.Window):
         if action is not None:
             try:
                 if NUM_PLAYERS == 2:
-                    state, reward, done = self.env.step([action, action2])
+                    states, rewards, done, info = self.env.step([action, action2])
                 else:
-                    state, reward, done = self.env.step([action])
+                    states, rewards, done, info = self.env.step([action])
             except Exception as e:
-                print(e)
+                import traceback
+                traceback.print_exception(e)
                 import ipdb; ipdb.set_trace()
+
             print(f"Player locations {self.env.players}")
-            print(f"Action rewards {reward}")
+            print(f"Action rewards {rewards}")
         self.acted = True
 
     def on_draw(self):
@@ -90,13 +97,13 @@ class Gathering(arcade.Window):
                 x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
                 y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
                 # figure out what color to draw the box
-                if self.env.contains_food(row, column):
+                if self.env._contains_food(row, column):
                     color = arcade.color.GREEN
-                elif self.env.contains_player(row, column, 1):
+                elif self.env._contains_player(row, column, 1):
                     color = arcade.color.BLUE
-                elif self.env.contains_player(row, column, 2):
+                elif self.env._contains_player(row, column, 2):
                     color = arcade.color.RED
-                elif self.env.contains_player_direction(row, column):
+                elif self.env._contains_player_direction(row, column):
                     color = arcade.color.GRAY
                 else:
                     color = arcade.color.BLACK
